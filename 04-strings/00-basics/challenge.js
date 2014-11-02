763ed16692c813cc5652f9907214de2b1cf1671a
Brandon Hopkins <hello@brandonwhopkins.com>
2014-11-02 11:14:15 -0700

    04-string updated

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -3,17 +3,18 @@ module.exports.formLetter = function(firstName, senderName, message) {
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-	  return bigString.substring(startA, endA) + bigString.substring(startB, endB);
+	return bigString.substring(startA, endA) + bigString(startB, endB);
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+	return text.indexOf (searchString);
 };
-
 module.exports.findLastMatch = function(text, searchString) {
-
+	return text.lastIndexOf (searchString);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-
+	firstMatch = text.indexOf(searchString) + searchString.length;
+	lastMatch = text.lastIndexOf(searchString);
+	return text.substring(firstMatch, lastMatch);
 };
