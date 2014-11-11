20e26f0f3c93251a9099a631c8c764eadebbb7d2
Brandon Hopkins <hello@brandonwhopkins.com>
2014-11-10 21:32:04 -0700

    08_Arrays updated

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,7 @@
-module.exports.newArray = undefined;
-
-module.exports.firstAndLast = undefined;
+module.exports.newArray = function(pen,notebook,mouse,ruler) {   
+  return [pen,notebook,mouse,ruler];
+};
+module.exports.firstAndLast = function(hat) {
+  var lastIndex = hat.length - 1;
+  return [hat[0],hat[lastIndex]]; 
+};
--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,15 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function (a){
+	a.reverse().unshift(1);
+	return a;
+};
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function (a){
+	
+	return a.join('+');
+};
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function (a){
+	var number = a.length;
+	arrayQuantityPlusOne =number+1;
+	return arrayQuantityPlusOne;
+};
